322a2a51065f48ed02d2291bb463470ef4d085c1
aya atef <ayaatef21ayaatef@outlook.com>
2024-09-05 13:37:17 +0300

    seeding data

---
+++ myshop.DataAccess/Seeds/DefaultRoles.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using myshop.DataAccess.Data.Constants;
+
+namespace myshop.DataAccess.Seeds
+{
+    public  class DefaultRoles
+    {
+        public static async Task SeedAsync(RoleManager<IdentityRole> roleManger)
+        {
+            if (!roleManger.Roles.Any())
+            {
+                await roleManger.CreateAsync(new IdentityRole(Roles.SuperAdmin.ToString()));
+                await roleManger.CreateAsync(new IdentityRole(Roles.Admin.ToString()));
+                await roleManger.CreateAsync(new IdentityRole(Roles.Basic.ToString()));
+            }
+        }
+    }
+}
