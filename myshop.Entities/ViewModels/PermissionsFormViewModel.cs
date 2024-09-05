f4f50a91c7fe2943c75d78d2ff66af8d565d5404
aya atef <ayaatef21ayaatef@outlook.com>
2024-09-05 13:43:57 +0300

    update permissions view model

---
+++ myshop.Entities/ViewModels/PermissionsFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myshop.Entities.ViewModels
+{
+    public class PermissionsFormViewModel
+    {
+        public string RoleId { get; set; } = string.Empty;
+        public string RoleName { get; set; }=string.Empty;
+        public List<CheckBoxViewModel> RoleCalims { get; set; }=new List<CheckBoxViewModel>();      
+    }
+}
