acaf7b88110b57b8d748f4d8f7d97e3437c46181
aya atef <ayaatef21ayaatef@outlook.com>
2024-09-05 13:35:18 +0300

    add user repository interface

---
+++ myshop.Entities/Repositories/IApplicationUserRepository.cs
@@ -0,0 +1,10 @@
+﻿
+
+using myshop.Entities.Models;
+
+namespace myshop.Entities.Repositories
+{
+    public interface IApplicationUserRepository:IGenericRepository<ApplicationUser>
+    {
+    }
+}
