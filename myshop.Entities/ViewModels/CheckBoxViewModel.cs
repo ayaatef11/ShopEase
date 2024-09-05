f203e02131605b51a915612ffe8ce99e58968d4a
aya atef <ayaatef21ayaatef@outlook.com>
2024-09-05 13:43:39 +0300

    update checkbox view model

---
+++ myshop.Entities/ViewModels/CheckBoxViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myshop.Entities.ViewModels
+{
+    public  class CheckBoxViewModel
+    {
+        public string DisplayValue { get; set; }=string.Empty;
+        public bool IsSelected { get; set; }
+    }
+}
