e2559087c3703bca83f79c68586a71bae36020f2
aya atef <ayaatef21ayaatef@outlook.com>
2024-09-05 13:32:29 +0300

    update generic repository

--- myshop.DataAccess/Implementation/GenericRepository.cs
+++ myshop.DataAccess/Implementation/GenericRepository.cs
@@ -1,13 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using myshop.Entities.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace myshop.DataAccess.Implementation
+namespace MyShop.DataAccess.Implementation
 {
     public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
