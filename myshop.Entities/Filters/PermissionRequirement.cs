e657ba4fb4ded3d7697d04ee69b00815b176486f
aya atef <ayaatef21ayaatef@outlook.com>
2024-09-05 13:36:57 +0300

    hashing the folder

---
+++ myshop.Entities/Filters/PermissionRequirement.cs
@@ -0,0 +1,13 @@
+﻿/*
+namespace MyShop.Filters
+{
+    public class PermissionRequirement : IAuthorizationRequirement
+    {
+        public string Permission { get; private set; }
+
+        public PermissionRequirement(string permission)
+        {
+            Permission = permission;
+        }
+    }
+}*/
