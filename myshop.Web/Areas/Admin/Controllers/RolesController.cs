4924275f32e89255164370dc1a822485fe8856df
aya atef <ayaatef21ayaatef@outlook.com>
2024-09-05 13:38:10 +0300

    update role controller

---
+++ myshop.Web/Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,84 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using myshop.DataAccess.Data.Constants;
+
+namespace PermissionBasedAuthorizationIntDotNet5.Controllers
+{
+   // [Authorize(Roles = "SuperAdmin")]
+    public class RolesController(RoleManager<IdentityRole> _roleManager, UserManager<IdentityUser> _userManager) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _roleManager.Roles.ToListAsync();
+            return View(roles);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddRole(RoleFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View("Index", await _roleManager.Roles.ToListAsync());
+
+            if (await _roleManager.RoleExistsAsync(model.Name))
+            {
+                ModelState.AddModelError("Name", "Role is exists!");
+                return View("Index", await _roleManager.Roles.ToListAsync());
+            }
+
+            await _roleManager.CreateAsync(new IdentityRole(model.Name.Trim()));
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> ManagePermissions(string roleId)
+        {
+            var role = await _roleManager.FindByIdAsync(roleId);
+
+            if (role == null)
+                return NotFound();
+
+            var roleClaims = _roleManager.GetClaimsAsync(role).Result.Select(c => c.Value).ToList();
+            var allClaims = Permissions.GenerateAllPermissions();
+            var allPermissions = allClaims.Select(p => new CheckBoxViewModel { DisplayValue = p }).ToList();
+
+            foreach (var permission in allPermissions)
+            {
+                if (roleClaims.Any(c => c == permission.DisplayValue))
+                    permission.IsSelected = true;
+            }
+
+            var viewModel = new PermissionsFormViewModel
+            {
+                RoleId = roleId,
+                RoleName = role.Name,
+                RoleCalims = allPermissions
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ManagePermissions(PermissionsFormViewModel model)
+        {
+            var role = await _roleManager.FindByIdAsync(model.RoleId);
+
+            if (role == null)
+                return NotFound();
+
+            var roleClaims = await _roleManager.GetClaimsAsync(role);
+
+            foreach (var claim in roleClaims)
+                await _roleManager.RemoveClaimAsync(role, claim);
+
+            var selectedClaims = model.RoleCalims.Where(c => c.IsSelected).ToList();
+
+            foreach (var claim in selectedClaims)
+                await _roleManager.AddClaimAsync(role, new Claim("Permission", claim.DisplayValue));
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
