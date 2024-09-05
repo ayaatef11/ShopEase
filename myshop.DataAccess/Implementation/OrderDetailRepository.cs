ee98f9d77c2a8614eb4b5d692f7d5abf09749480
aya atef <ayaatef21ayaatef@outlook.com>
2024-09-05 13:32:59 +0300

    update order details repo

--- myshop.DataAccess/Implementation/OrderDetailRepository.cs
+++ myshop.DataAccess/Implementation/OrderDetailRepository.cs
@@ -1,12 +1,6 @@
-﻿using myshop.Entities.Models;
-using myshop.Entities.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace myshop.DataAccess.Implementation
+namespace MyShop.DataAccess.Implementation
 {
     public class OrderDetailRepository : GenericRepository<OrderDetail>, IOrderDetailRepository
     {
