d1b6d086ab2e9c87849e8d05b9a58d3f56e57d09
aya atef <ayaatef21ayaatef@outlook.com>
2024-09-05 13:34:27 +0300

    add web global file

---
+++ myshop.Web/GLOBALS.cs
@@ -0,0 +1,14 @@
+﻿global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+global using myshop.Entities.Models;
+global using myshop.Entities.Repositories;
+global using myshop.Entities.ViewModels;
+global using myshop.Utilities;
+global using Stripe.Checkout;
+global using System.Security.Claims;
+global using System.ComponentModel.DataAnnotations;
+global using System.Text;
+global using System.Text.Encodings.Web;
+global using Microsoft.AspNetCore.Authentication;
+global using Microsoft.AspNetCore.Identity;
